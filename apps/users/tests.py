7e3112f35205507f4aec9cccabdb9bec028159f2
cosmos2 <playnstop.s@gmail.com>
2021-08-10 22:58:06 +0900

    [ADD] users test

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from model_bakery import baker
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class UserViewSetTest(APITestCase):
+    LIST_URL = "/users/users/"
+
+    def test_list(self):
+        user = baker.make(User)
+        self.client.force_authenticate(user)
+
+        users = baker.make(User, _quantity=10)
+
+        response = self.client.get(self.LIST_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], 11)
